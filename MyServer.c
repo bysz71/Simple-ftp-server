614cda8ba2770e8a6cac5f325dafe1445ab4ada3
scottszb1987 <scottszb1987@hotmail.com>
2015-03-16 14:42:06 +1300

    v1.6
    structure alternated, STOR and RETR function required

--- MyServer.c
+++ MyServer.c
@@ -6,6 +6,7 @@
 #include <winsock.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <time.h>
 #define WSVERS MAKEWORD(2,0) 
 //for wsastartup() use only, make 2 byte -> 16bit word
 WSADATA wsadata;
@@ -22,7 +23,6 @@ char rfc[20][4];
 
 
 
-
 main(int argc, char *argv[]){
 	strcpy(rfc[0],"SYST");
 	strcpy(rfc[1],"USER");
@@ -35,11 +35,6 @@ main(int argc, char *argv[]){
 	struct sockaddr_in localaddr,remoteaddr,data_addr_act;
 	SOCKET s, ns, s_data_act;
 	
-	int act_ip[4];
-	unsigned char act_port[2];
-	int port_dec, connect_result;
-	char addr_data[20];
-	
 	char send_buffer[BUFFERSIZE],receive_buffer[BUFFERSIZE];
 	char temp[BUFFERSIZE];
 	char bool;
@@ -50,47 +45,46 @@ main(int argc, char *argv[]){
 	memset(&localaddr,0,sizeof(localaddr));
 	memset(&remoteaddr,0,sizeof(remoteaddr));	//init local and remote addresses
 	
-	DIR *dp;
-	struct dirent *ep;
+	
 
 
-//winsock init	
-	printf("\nInitialising Winsock ...\n");
+	//winsock init	
+	printf("\nSERVER : Initialising Winsock ...\n");
 	if(WSAStartup(WSVERS, &wsadata) != 0){
 		WSACleanup();
 		printf("WSAStartup failed. Error Code : %d", WSAGetLastError());
 		exit(1);
 	}
-	printf("Initialised.\n");
+	printf("SERVER : Initialised.\n");
 
-//SOCKET
+	//SOCKET
 	s = socket(PF_INET, SOCK_STREAM, 0);	//init s socket(A/P family[PF_INET for tcp] , transport[SOCK_STREAM for tcp] , protocol specified [0 for no specification])
 	if (s < 0) {
-		printf("socket failed\n");
+		printf("SERVER : socket failed\n");
 		exit(1);
 	}
 	localaddr.sin_family = AF_INET;	//address family set for tcp
 	if (argc == 2) {	//decision on using specified port or default port
 		localaddr.sin_port = htons((u_short)atoi(argv[1]));
-		printf("Communication port is specified to %s. \n",argv[1]);
+		printf("SERVER : Communication port is specified to %s. \n",argv[1]);
 	}else{
 		localaddr.sin_port = htons(cport);
-		printf("No port specified, use default port %d.\n",cport);
+		printf("SERVER : No port specified, use default port %d.\n",cport);
 	}
 	localaddr.sin_addr.s_addr = INADDR_ANY;	//s_addr, 32 bit unsigned long IP addr, INADDR_ANY -> 0,0,0,0 local
 	
-//BIND: bind socket with local address
+	//BIND: bind socket with local address
 	if (bind(s,(struct sockaddr *)(&localaddr),sizeof(localaddr))!=0){
-		printf("Bind failed!\n");
+		printf("SERVER : Bind failed!\n");
 		exit(0);
 	}
-	printf("binding succeeded.\n");
+	printf("SERVER : binding succeeded.\n");
 
-//LISTEN
+	//LISTEN
 	listen(s,5);
-	printf("Listening...\n");
+	printf("SERVER : Listening...\n");
 
-//infinite loop
+	//infinite loop
 	while(1){
 		addrlen = sizeof(remoteaddr);
 		ns = accept(s,(struct sockaddr *)(&remoteaddr),&addrlen);
@@ -98,106 +92,114 @@ main(int argc, char *argv[]){
 			sprintf(send_buffer,"421 Service not available, closing control connection\n");
 			break;
 		}
-		printf("accepted a connection from client IP %s port %d \n",inet_ntoa(remoteaddr.sin_addr),ntohs(localaddr.sin_port));
+		printf("SERVER : accepted a connection from client IP %s port %d \n",inet_ntoa(remoteaddr.sin_addr),ntohs(localaddr.sin_port));
 		//inet_ntoa converts Ipv4 address into ASCII string in internet standard dotted-decimal format
 		//ntohs converts a u_short from TCP/IP network byte order to host byte order (format conversion blah blah)
 
-//system info feed if 421 not happen
+		//system info feed if 421 not happen
 		sprintf(send_buffer,"220 Microsoft FTP service\n");
 		bytes = send(ns,send_buffer,strlen(send_buffer),0);
 		if(bytes<0)break;
 
-//log in session
 		while(1){
 			bytes = recv_msg(receive_buffer,ns);
 			if(bytes < 0)break;
-			
 			memset(&send_buffer,0,BUFFERSIZE);
+			
 			if(strncmp(receive_buffer,"USER",4) == 0){
 				sprintf(send_buffer,"331 password required\n");
 				bytes = send(ns,send_buffer,strlen(send_buffer),0);
-				continue;
+				if(bytes < 0)break;
 			}
+			
 			if(strncmp(receive_buffer,"PASS",4) == 0){
 				sprintf(send_buffer,"230 logged in\n");
 				bytes = send(ns,send_buffer,strlen(send_buffer),0);
-				break;
+				if(bytes < 0)break;
 			}
-		}
+			
+			if(strncmp(receive_buffer,"QUIT",4) == 0){
+				sprintf(send_buffer,"221 Service closing control connection\n");
+				bytes = send(ns,send_buffer,strlen(send_buffer),0);
+				if(bytes < 0)break;
+				exit(1);
+			}
+	
+			if(strncmp(receive_buffer,"PORT",4) == 0){
+				int act_ip[6], port_dec, connect_result;
+				unsigned char act_port[2];
+				char data_addr[20];
+				
+				sscanf(receive_buffer,"PORT %d,%d,%d,%d,%d,%d",&act_ip[0],&act_ip[1],&act_ip[2],&act_ip[3],&act_port[0],&act_port[1]);
+				sscanf(receive_buffer,"PORT %d,%d,%d,%d,%d,%d",&act_ip[0],&act_ip[1],&act_ip[2],&act_ip[3],&act_ip[4],&act_ip[5]);
+				sprintf(data_addr,"%d.%d.%d.%d",act_ip[0],act_ip[1],act_ip[2],act_ip[3]);
+				port_dec = (act_port[0] << 8) + act_port[1];
+				data_addr_act.sin_port = htons(port_dec);			
+				data_addr_act.sin_addr.s_addr = inet_addr(data_addr);
+				data_addr_act.sin_family = AF_INET;
+				s_data_act = socket(AF_INET, SOCK_STREAM, 0);
+				if(s_data_act == INVALID_SOCKET){
+					printf("SERVER : socket function failed with error:%d \n",WSAGetLastError());
+					WSACleanup();
+					return 1;
+				}
+				connect_result = connect(s_data_act, (struct sockaddr *) & data_addr_act, sizeof(data_addr_act));
+				if(connect_result == SOCKET_ERROR){
+					printf("SERVER : connect function failed with error:%d \n",WSAGetLastError());
+					connect_result == closesocket(s_data_act);
+					if(connect_result == SOCKET_ERROR)
+						printf("SERVER : close socket function failed with error:%d \n",WSAGetLastError());
+					WSACleanup();
+					return 1;
+				}
+				printf("SERVER : data connection established.\n");
+				sprintf(send_buffer,"200 PORT command successful\n");
+				bytes = send(ns,send_buffer,strlen(send_buffer),0);
+				if(bytes < 0) break;
+			}
 
-//port session
-		bytes = recv_msg(receive_buffer,ns);
-		if(bytes < 0) break;
-		if(strncmp(receive_buffer,"PORT",4) == 0){
-			//str_cut(temp,receive_buffer,5,strlen(receive_buffer));
-			//printf(temp);
-			//sscanf(temp, "%d,%d,%d,%d,%d,%d",&act_ip[0],&act_ip[1],&act_ip[2],&act_ip[3],&act_port[0],&act_port[1]);
-			//sprintf(addr_data,"%d.%d.%d.%d",act_ip[0],act_ip[1],act_ip[2],act_ip[3]);
-			//printf("%s",addr_data);
-			sscanf(receive_buffer, "PORT %d,%d,%d,%d,%d,%d",&act_ip[0],&act_ip[1],&act_ip[2],&act_ip[3],&act_port[0],&act_port[1]);
-			port_dec = (act_port[0] << 8) + act_port[1];
-			data_addr_act.sin_port = htons(port_dec);			
-			data_addr_act.sin_addr.s_addr = inet_addr("192.168.1.5");
-			data_addr_act.sin_family = AF_INET;
-			s_data_act = socket(AF_INET, SOCK_STREAM, 0);
-			if(s_data_act == INVALID_SOCKET){
-				printf("socket function failed with error:%d \n",WSAGetLastError());
-				WSACleanup();
-				return 1;
-			}
-			connect_result = connect(s_data_act, (struct sockaddr *) & data_addr_act, sizeof(data_addr_act));
-			if(connect_result == SOCKET_ERROR){
-				printf("connect function failed with error:%d \n",WSAGetLastError());
-				connect_result == closesocket(s_data_act);
-				if(connect_result == SOCKET_ERROR)
-					printf("close socket function failed with error:%d \n",WSAGetLastError());
-				WSACleanup();
-				return 1;
-			}
-			printf("data connection established\n");
-			sprintf(send_buffer,"200 PORT command successful\n");
-			bytes = send(ns,send_buffer,strlen(send_buffer),0);
-			if(bytes < 0) break;
-		}
-		
-//command session
-//windows ftp client seems do not support passive mode Orz		
-		while(1){
-			bytes = recv_msg(receive_buffer,ns);
-			if(bytes < 0) break;
 			if(strncmp(receive_buffer,"LIST",4) == 0){
-				sprintf(send_buffer,"150 opening data connection for file list...\n");
+				DIR *dp;
+				struct dirent *ep;
+				sprintf(send_buffer,"125 data connection already open, transfer starting\n");
 				bytes = send(ns,send_buffer,strlen(send_buffer),0);
 				if(bytes < 0) break;
 				dp = opendir("./");
 				if(dp != NULL){
 					while(ep = readdir(dp)){
-						puts(ep->d_name);
-						//sprintf(send_buffer,ep->d_name);
-						//strcat(send_buffer,"\n");
-						//bytes = send(s_data_act, send_buffer,strlen(send_buffer),0);
-						//if(bytes < 0) break;
+						//puts(ep->d_name);
+						sprintf(temp,"200\t");
+						strcat(temp,ep->d_name);
+						strcat(temp,"\n");
+						sprintf(send_buffer,temp);
+						bytes = send(s_data_act, send_buffer,strlen(send_buffer),0);
+						if(bytes < 0) break;						
 					}
-					printf("aaa\n");
 					(void) closedir (dp);
 				}else
-					perror("couldn't open the directory");
-				printf("bbb\n");
+					perror("SERVER : couldn't open the directory.");
+				closesocket(s_data_act);
 				sprintf(send_buffer,"226 closing data connection. Requested file action successful.\n");
 				bytes = send(ns,send_buffer,strlen(send_buffer),0);
 				if(bytes < 0) break;
-			}else{
-				sprintf(send_buffer,"150 hahahah");
-				bytes = send(ns,send_buffer,strlen(send_buffer),0);
-				if(bytes < 0) break;
 			}
+			
+			if(strncmp(receive_buffer,"STOR",4) == 0){
+				
+			}
+			
+			if(strncmp(receive_buffer,"RETR",4) == 0){
+				
+			}
+			
+			
 		}
 
 
 
 		closesocket(ns);
-		printf("disconnected from %s \n", inet_ntoa(remoteaddr.sin_addr));
-		printf("exit?(y/n)");
+		printf("SERVER : disconnected from %s \n", inet_ntoa(remoteaddr.sin_addr));
+		printf("SERVER : exit?(y/n)");
 		bool = getchar();
 		if(bool=='y')exit(1);
 		
--- test.c
+++ test.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 
 int main(){
-	char a[] = "PORT 192,168,1,5,32,155";
-	int b[6];
-	sscanf(a,"PORT %d,%d,%d,%d,%d,%d",&b[0],&b[1],&b[2],&b[3],&b[4],&b[5]);
-	printf("%d,%d,%d,%d",b[0],b[1],b[2],b[3]);
+	char fake[] = "PORT 192,168,1,5,10,177";
+	//int f[4];
+	int f[6];
+	unsigned char fc[2];
+	char tmp[40];
+	//sscanf(fake,"PORT %d,%d,%d,%d,%d,%d",&f[0],&f[1],&f[2],&f[3],(int*)&fc[0],(int*)&fc[1]);
+	sscanf(fake,"PORT %d,%d,%d,%d,%d,%d",&f[0],&f[1],&f[2],&f[3],&f[4],&f[5]);
+	printf("watch:\n");
+	printf("aaa %d.%d.%d.%d",f[0],f[1],f[2],f[3]);
 	
 }
